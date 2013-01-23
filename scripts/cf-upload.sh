9c61ad38200e2a41bd24a6eb448e9828768a11f5
Joseph W. Breu <breu@breu.org>
2013-01-23 10:22:19 -0600

    update

--- scripts/cf-upload.sh
+++ scripts/cf-upload.sh
@@ -5,7 +5,7 @@
 TMPFILE=`mktemp`
 
 # SOURCE API AND USER_NAME HERE
-source files/cloud-creds
+source ~/jenkins-build/files/cloud-creds
 
 curl -s -D - -H "X-Auth-Key: $API" -H "X-Auth-User: $USER_NAME" https://api.mosso.com/auth > $TMPFILE
 
