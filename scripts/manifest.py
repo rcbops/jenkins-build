f9ee45a82b9a43e5ca95e97e7f673a7531bd343d
Christopher H. Laco <claco@chrislaco.com>
2013-02-20 14:34:11 -0500

    Add MD5 for tarbal to cookbook manifest

--- scripts/manifest.py
+++ scripts/manifest.py
@@ -3,6 +3,7 @@
 from optparse import OptionParser
 import json
 import sys
+import datetime
 
 
 def main():
@@ -12,14 +13,14 @@ def main():
     try:
         (options, args) = parser.parse_args(sys.argv[1:])
 
-        if len(args) != 3:
-            parser.error("manifest, version and url are required")
+        if len(args) != 4:
+            parser.error("manifest, version, url, and md5 are required")
 
         source = open(args[0], 'r+')
 
         manifest = json.load(source)
 
-        add_version(manifest, args[1], args[2])
+        add_version(manifest, args[1], args[2], args[3])
 
         source.seek(0)
         source.write(json.dumps(manifest, sort_keys=True, indent=2))
@@ -30,9 +31,13 @@ def main():
         print >> sys.stderr, e
 
 
-def add_version(manifest, version, url):
+def add_version(manifest, version, url, md5):
     manifest['current'] = version
-    manifest['versions'][version] = {"url": url}
+    manifest['versions'][version] = {
+        "url": url,
+        "md5": md5,
+        "updated": datetime.datetime.utcnow().isoformat()
+    }
 
     return
 
