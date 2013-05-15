19ba2228195601c9a781e95ad44bf83032b808e3
solo4572 <jacob.wagner@rackspace.com>
2013-05-15 13:34:56 -0500

    modularized the build env script, added default env, added ability to build multiple of the same env

--- qa/scripts/bash/jenkins/build-chef-environment.sh
+++ qa/scripts/bash/jenkins/build-chef-environment.sh
@@ -1,84 +1,103 @@
 #!/bin/bash
 
+function set_environment_files(){
+    echo "Building ${NAME}-${OS_DISTRO}-${FEATURE_SET} chef environment for OpenStack component $PACKAGE_COMPONENT"
+    
+    # set filenames
+    TEMPLATE_FILENAME="/var/lib/jenkins/rcbops-qa/chef-cookbooks/environments/templates/${OS_DISTRO}-${FEATURE_SET}.json"
+    ENVIRONMENT_FILENAME="/var/lib/jenkins/rcbops-qa/chef-cookbooks/environments/${NAME}-${OS_DISTRO}-${FEATURE_SET}.json"
+
+    ## copy the environment file to the proper directory
+    echo "Copying template to environment..."
+    cp $TEMPLATE_FILENAME $ENVIRONMENT_FILENAME
+}
+
+function build_chef_environment() {
+    echo "Set Knife Environment..."
+    knife environment from file $ENVIRONMENT_FILENAME
+}
+
+function build_default() {
+
+    # build environment files from templates
+    set_environment_files
+
+    ## replace the lines we are looking for
+    echo "Replacing template values with real values..."
+    result=`sed -i 's/<NAME>/'$${NAME}-${OS_DISTRO}-${FEATURE_SET}'/g' $ENVIRONMENT_FILENAME`
+    result=`sed -i 's/<PACKAGE_COMPONENT>/'${PACKAGE_COMPONENT}'/g' $ENVIRONMENT_FILENAME`
+
+    # build chef environment
+    build_chef_environment
+}
+
 function build_glance_cf() {
 
-    echo "Building ${OS_DISTRO}-${FEATURE_SET} chef environment for OpenStack component $PACKAGE_COMPONENT"
-
-    # set filenames
-    template_filename="/var/lib/jenkins/rcbops-qa/chef-cookbooks/environments/templates/${OS_DISTRO}-${FEATURE_SET}.json"
-    environment_filename="/var/lib/jenkins/rcbops-qa/chef-cookbooks/environments/${OS_DISTRO}-${FEATURE_SET}.json"
+    # build environment files from templates
+    set_environment_files
     
     # source our secret file with hidden info
     source ~/source_files/CLOUD_FILES_AUTH.sh
 
-    ## copy the environment file to the proper directory
-    echo "Copying template to environment..."
-    cp $template_filename $environment_filename
-
     ## replace the lines we are looking for
     echo "Replacing template values with real values..."
-    result=`sed -i 's/<PACKAGE_COMPONENT>/'${PACKAGE_COMPONENT}'/g' $environment_filename`
-    result=`sed -i 's/<TENANT_ID>/'${TENANT_ID}'/g' $environment_filename`
-    result=`sed -i 's/<TENANT_NAME>/'${TENANT_NAME}'/g' $environment_filename`
-    result=`sed -i 's/<TENANT_PASSWORD>/'${TENANT_PASSWORD}'/g' $environment_filename`
+    result=`sed -i 's/<NAME>/'${NAME}-${OS_DISTRO}-${FEATURE_SET}'/g' $ENVIRONMENT_FILENAME`
+    result=`sed -i 's/<PACKAGE_COMPONENT>/'${PACKAGE_COMPONENT}'/g' $ENVIRONMENT_FILENAME`
+    result=`sed -i 's/<TENANT_ID>/'${TENANT_ID}'/g' $ENVIRONMENT_FILENAME`
+    result=`sed -i 's/<TENANT_NAME>/'${TENANT_NAME}'/g' $ENVIRONMENT_FILENAME`
+    result=`sed -i 's/<TENANT_PASSWORD>/'${TENANT_PASSWORD}'/g' $ENVIRONMENT_FILENAME`
 
-    echo "Set Knife Environment..."
-    knife environment from file $environment_filename
+    # build chef environment
+    build_chef_environment
 }
 
 function build_keystone_ldap() {
-    echo "Building ${OS_DISTRO}-${FEATURE_SET} chef environment for OpenStack component $PACKAGE_COMPONENT"
+
+    # build environment files from templates
+    set_environment_files
 
     # Source the file that has our environment variables
     source ~/source_files/LDAP.sh
 
     export LDAP_IP=`knife search node 'role:qa-openldap-ubuntu' | grep IP | awk '{print $2}'`
 
-    # set filenames
-    template_filename="/var/lib/jenkins/rcbops-qa/chef-cookbooks/environments/templates/${OS_DISTRO}-${FEATURE_SET}.json"
-    environment_filename="/var/lib/jenkins/rcbops-qa/chef-cookbooks/environments/${OS_DISTRO}-${FEATURE_SET}.json"
-
-    ## copy the environment file to the proper directory
-    echo "Copying template to environment..."
-    cp $template_filename $environment_filename
-
-
     ## replace the lines we are looking for
     echo "Replacing template values with real values..."
-    result=`sed -i 's/<PACKAGE_COMPONENT>/'${PACKAGE_COMPONENT}'/g' $environment_filename`
-    result=`sed -i 's/<LDAP_IP>/'${LDAP_IP}'/g' $environment_filename`
-    result=`sed -i 's/<LDAP_ADMIN_PASS>/'${LDAP_ADMIN_PASS}'/g' $environment_filename`
+    result=`sed -i 's/<NAME>/'${NAME}-${OS_DISTRO}-${FEATURE_SET}'/g' $ENVIRONMENT_FILENAME`
+    result=`sed -i 's/<PACKAGE_COMPONENT>/'${PACKAGE_COMPONENT}'/g' $ENVIRONMENT_FILENAME`
+    result=`sed -i 's/<LDAP_IP>/'${LDAP_IP}'/g' $ENVIRONMENT_FILENAME`
+    result=`sed -i 's/<LDAP_ADMIN_PASS>/'${LDAP_ADMIN_PASS}'/g' $ENVIRONMENT_FILENAME`
 
-    echo "Set Knife Environment..."
-    knife environment from file $environment_filename
+    # build chef environment
+    build_chef_environment
 }
 
 function build_nova_quantum() {
+
     echo "NOT IMPLEMENTED"
 }
 
 function build_opencenter() {
-    echo "Building ${OS_DISTRO}-${FEATURE_SET} chef environment for OpenStack component $PACKAGE_COMPONENT"
-    # Set temp file and perm file
-    template_filename="/var/lib/jenkins/rcbops-qa/chef-cookbooks/environments/templates/${OS_DISTRO}-${FEATURE_SET}.json"
-    environment_filename="/var/lib/jenkins/rcbops-qa/chef-cookbooks/environments/${OS_DISTRO}-${FEATURE_SET}.json"
 
-    ## copy the environment file to the proper directory
-    echo "Copying template to environment..."
-    cp $template_filename $environment_filename
+    # build environment files from templates
+    set_environment_files
 
-    echo "Set Knife Environment..."
-    knife environment from file $environment_filename
+    # build chef environment
+    build_chef_environment
 }
 
+#BEGIN SCRIPT
+
 if [[ "$#" -eq 0 ]]; then
-    echo "Usage: $0 [-h help] -p package_component -d os_distro -f feature_set" >&2
+    echo "Usage: $0 [-h help] -n name -p package_component -d os_distro -f feature_set" >&2
     exit
 fi
 
-while getopts "p:d:f:h" OPTION;
+while getopts "n:p:d:f:h" OPTION;
 do
     case $OPTION in
+        n)  NAME=$OPTARG
+            ;;
         p)  PACKAGE_COMPONENT=$OPTARG
             ;;
         d)  OS_DISTRO=$OPTARG
@@ -87,6 +106,7 @@ do
             ;;
         h)  echo "Usage: $0" >&2
             echo "  -h   Return this help information" >&2
+            echo "  -n   The name to prepend to the environment (i.e. test, yourname, etc)"
             echo "  -p   The package component for OpenStack (i.e. folsom, grizzly, etc). REQUIRED" >&2
             echo "  -d   The operating system distribution that OpenStack will be built on (i.e. precise, centos, redhat). REQUIRED" >&2
             echo "  -f   The feature set of OpenStack to build (i.e. glance-cf, keystone-ldap, etc). REQUIRED" >&2
@@ -96,6 +116,7 @@ do
 done
 
 case $FEATURE_SET in
+    'default')          build_default;;
     'glance-cf')        build_glance_cf;;
     'keystone-ldap')    build_keystone_ldap;;
     'nova-quantum')     build_nova_quantum;;
