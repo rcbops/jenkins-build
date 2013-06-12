8151d9bf653fe7462ac7f1f9398e5cc4cf92356e
Cameron Lopez <cameronjlopez@gmail.com>
2013-06-12 13:25:15 -0500

    add centos packages

--- qa/scripts/python/bare_metal/tests/tempest_cluster.py
+++ qa/scripts/python/bare_metal/tests/tempest_cluster.py
@@ -115,7 +115,7 @@ print "## Setting up tempest on chef server ##"
 if results.os_distro == "precise":
     packages = "apt-get install python-pip libmysqlclient-dev libxml2-dev libxslt1-dev python2.7-dev libpq-dev -y"
 else:
-    packages = "yum install python-pip -y"
+    packages = "yum install python-pip python-lxml gcc python-devel openssl-devel mysql-devel postgresql-devel -y; easy_install pip;"
 commands = ["git clone https://github.com/openstack/tempest.git -b stable/%s --recursive" % (results.tempest_version),
             packages,
             "easy_install -U distribute",
