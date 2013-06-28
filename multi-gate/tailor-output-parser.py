a4f21e312bff4c9bafe648f50d8f903b7e06e7ed
rackerjoe <breu@breu.org>
2013-06-28 15:28:22 -0500

    fixup the filename parser

--- multi-gate/tailor-output-parser.py
+++ multi-gate/tailor-output-parser.py
@@ -8,7 +8,7 @@ f = sys.stdin
 dataMap = yaml.safe_load(sys.stdin)
 
 for x in dataMap:
-  file_name =  os.path.relpath(x,os.getcwd() + "/")
+  file_name =  os.path.join(os.path.basename(os.getcwd()),os.path.relpath(x,os.getcwd()))
   for y in dataMap[x]:
     print "%s : %s : %d : %s : %s" % (file_name, y[':level'][1:],
                                       y[':line'], y[':type'], y[':message'])
