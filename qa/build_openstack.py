0036e6b959e968f70cae159025962e7b2dd17624
john5223 <curran736@gmail.com>
2013-08-07 09:36:13 -0500

    else node

--- qa/build_openstack.py
+++ qa/build_openstack.py
@@ -207,7 +207,7 @@ else:
             node.save()
 
             if 'run_list' in b:
-                node = Node(node.name, api=rpcsqa.remote_chef_client(env)) if args.remote_chef
+                node = Node(node.name, api=rpcsqa.remote_chef_client(env)) if args.remote_chef else node
                 node.run_list = b['run_list']
                 node.save()
                 print "Running chef client for %s" % node
