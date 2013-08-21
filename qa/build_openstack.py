3d519c8979f770e98f57660fd47c99e6be312728
cjoelrun <cameronjlopez@gmail.com>
2013-08-21 11:59:41 -0500

    set environment

--- qa/build_openstack.py
+++ qa/build_openstack.py
@@ -235,6 +235,7 @@ def main():
                     # Reacquires node if using remote chef
                     node = Node(node.name, api=api)
                     node.run_list = b['run_list']
+                    node.enviroment = env
                     node.save()
                     print "Running chef client for %s" % node
                     print node.run_list
