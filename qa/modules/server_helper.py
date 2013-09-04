cbdcec6601b27e2c78826e277e908507d0648c4e
john5223 <curran736@gmail.com>
2013-09-04 16:59:49 -0500

    quiet

--- qa/modules/server_helper.py
+++ qa/modules/server_helper.py
@@ -25,7 +25,7 @@ def run_remote_ssh_cmd(server_ip, user, password, remote_cmd, quiet=False):
     f = chan.makefile()
     chan.exec_command(remote_cmd)
     for line in f:
-        if quiet:
+        if not quiet:
             sys.stdout.write(line)
         output.write(line)
     exit_status = chan.recv_exit_status()
