45072d26c30ae5f6bd615a2a9c4d5211facb4c46
cjoelrun <cameronjlopez@gmail.com>
2013-09-30 22:14:18 -0500

    remove cinder from computes

--- qa/modules/Builds.py
+++ qa/modules/Builds.py
@@ -21,47 +21,47 @@ class Build(object):
     """
 
     def __init__(self, name, role, qa, branch, pre_commands=[],
-                 post_commands=[]):
-        self.name = name
-        self.role = role
-        self.qa = qa
-        self.branch = branch
-        self.pre_commands = pre_commands
-        self.post_commands = post_commands
-        self.status = "Prebuild"
+		 post_commands=[]):
+	self.name = name
+	self.role = role
+	self.qa = qa
+	self.branch = branch
+	self.pre_commands = pre_commands
+	self.post_commands = post_commands
+	self.status = "Prebuild"
 
     def build(self):
-        self.status = "Building"
-        self.preconfigure()
-        self.status = "Building"
-        self.apply_role()
-        self.postconfigure()
+	self.status = "Building"
+	self.preconfigure()
+	self.status = "Building"
+	self.apply_role()
+	self.postconfigure()
 
     def preconfigure(self):
-        self.status = "Preconfigure"
-        self._run_commands(self.pre_commands)
+	self.status = "Preconfigure"
+	self._run_commands(self.pre_commands)
 
     def apply_role(self):
-        self.status = "Preconfigure"
-        raise NotImplementedError
+	self.status = "Preconfigure"
+	raise NotImplementedError
 
     def postconfigure(self):
-        self.status = "Postconfigure"
-        self._run_commands(self.post_commands)
+	self.status = "Postconfigure"
+	self._run_commands(self.post_commands)
 
     def _run_commands(self, commands):
-        raise NotImplementedError
+	raise NotImplementedError
 
     def _map_kwargs(self, kwargs):
-        for key in kwargs.keys():
-            kwargs[key] = getattr(self, kwargs[key])
-        return kwargs
+	for key in kwargs.keys():
+	    kwargs[key] = getattr(self, kwargs[key])
+	return kwargs
 
     def __str__(self):
-        return ("Build: {0} - Role: {1} - "
-                "pre: {2} - post {3}").format(self.name, self.role,
-                                              self.pre_commands,
-                                              self.post_commands)
+	return ("Build: {0} - Role: {1} - "
+		"pre: {2} - post {3}").format(self.name, self.role,
+					      self.pre_commands,
+					      self.post_commands)
 
 
 class ChefBuild(Build):
@@ -70,81 +70,81 @@ class ChefBuild(Build):
     """
 
     def __init__(self, name, role, qa, branch, env, api=None,
-                 pre_commands=[], post_commands=[]):
-        super(ChefBuild, self).__init__(name, role, qa, branch,
-                                        pre_commands=pre_commands,
-                                        post_commands=post_commands)
-        self.environment = env
-        self.run_list = self._run_list_map(role)
-        self.api = api or chef_api()
-        self.cookbooks = self.cookbook_branch(branch)
+		 pre_commands=[], post_commands=[]):
+	super(ChefBuild, self).__init__(name, role, qa, branch,
+					pre_commands=pre_commands,
+					post_commands=post_commands)
+	self.environment = env
+	self.run_list = self._run_list_map(role)
+	self.api = api or chef_api()
+	self.cookbooks = self.cookbook_branch(branch)
 
     def _run_list_map(self, role):
-        return {
-            "chef_server": [],
-            "compute": ['role[single-compute]', 'role[cinder-all]'],
-            "directory_server": ['role[qa-openldap]'],
-            "controller1": ['role[ha-controller1]', 'role[cinder-all]'],
-            "controller2": ['role[ha-controller2]']
-        }[role]
+	return {
+	    "chef_server": [],
+	    "compute": ['role[single-compute]'],
+	    "directory_server": ['role[qa-openldap]'],
+	    "controller1": ['role[ha-controller1]', 'role[cinder-all]'],
+	    "controller2": ['role[ha-controller2]']
+	}[role]
 
     def bootstrap(self):
-        self.qa.remove_chef(self.name)
-        self.qa.bootstrap_chef(self.name, self.api.server)
+	self.qa.remove_chef(self.name)
+	self.qa.bootstrap_chef(self.name, self.api.server)
 
     def _run_commands(self, commands):
-        for command in commands:
-            #If its a string run on remote server
-            if isinstance(command, str):
-                self.qa.run_command_on_node(Node(self.name), command)
-            if isinstance(command, dict):
-                try:
-                    func = getattr(self.qa, command['function'])
-                    kwargs = self._map_kwargs(command['kwargs'])
-                    func(**kwargs)
-                except:
-                    print traceback.print_exc()
-                    sys.exit(1)
-            #elif function run the function
-            elif hasattr(command, '__call__'):
-                command()
+	for command in commands:
+	    #If its a string run on remote server
+	    if isinstance(command, str):
+		self.qa.run_command_on_node(Node(self.name), command)
+	    if isinstance(command, dict):
+		try:
+		    func = getattr(self.qa, command['function'])
+		    kwargs = self._map_kwargs(command['kwargs'])
+		    func(**kwargs)
+		except:
+		    print traceback.print_exc()
+		    sys.exit(1)
+	    #elif function run the function
+	    elif hasattr(command, '__call__'):
+		command()
 
     def preconfigure(self):
-        self.status = "Preconfigure"
-        node = Node(self.name)
-        node['in_use'] = self.role
-        node.chef_environment = self.environment
-        node.save()
-        if self.api.remote and not self.role in ["chef_server", "openldap"]:
-            self.bootstrap()
-        super(ChefBuild, self).preconfigure()
-        if self.role is "chef_server":
-            # This should be done in the chef_server build
-            self.api.remote = self\
-                    .qa.remote_chef_client(self.environment)
+	self.status = "Preconfigure"
+	node = Node(self.name)
+	node['in_use'] = self.role
+	node.chef_environment = self.environment
+	node.save()
+	if self.api.remote and not self.role in ["chef_server", "openldap"]:
+	    self.bootstrap()
+	super(ChefBuild, self).preconfigure()
+	if self.role is "chef_server":
+	    # This should be done in the chef_server build
+	    self.api.remote = self\
+		    .qa.remote_chef_client(self.environment)
 
     def apply_role(self):
-        node = Node(self.name, api=self.api.api)
-        node.run_list = self._run_list_map(self.role)
-        node.chef_environment = self.environment
-        node.save()
+	node = Node(self.name, api=self.api.api)
+	node.run_list = self._run_list_map(self.role)
+	node.chef_environment = self.environment
+	node.save()
 
-        if node.run_list:
-            print "Running chef client for %s" % node
-            print node.run_list
-            chef_client = self.qa.run_chef_client(node, num_times=2)
-            if not chef_client['success']:
-                print "chef_client run failed"
-                self.status = "Failure"
-                raise Exception
+	if node.run_list:
+	    print "Running chef client for %s" % node
+	    print node.run_list
+	    chef_client = self.qa.run_chef_client(node, num_times=2)
+	    if not chef_client['success']:
+		print "chef_client run failed"
+		self.status = "Failure"
+		raise Exception
 
     def cookbook_branch(self, branch):
-        return [
-            {
-                "url": "https://github.com/rcbops/chef-cookbooks.git",
-                "branch": branch
-            }
-        ]
+	return [
+	    {
+		"url": "https://github.com/rcbops/chef-cookbooks.git",
+		"branch": branch
+	    }
+	]
 
 
 class DeploymentBuild(Build):
@@ -153,23 +153,23 @@ class DeploymentBuild(Build):
     """
 
     def __init__(self, name, builds=[], pre_commands=[], post_commands=[]):
-        self.name = name
-        self.builds = builds
-        self.pre_commands = pre_commands
-        self.post_commands = post_commands
+	self.name = name
+	self.builds = builds
+	self.pre_commands = pre_commands
+	self.post_commands = post_commands
 
     def build(self):
-        self.preconfigure()
-        for build in self.builds:
-            build.build()
-        self.postconfigure()
+	self.preconfigure()
+	for build in self.builds:
+	    build.build()
+	self.postconfigure()
 
     def append(self, item):
-        self.builds.append(item)
+	self.builds.append(item)
 
     def __str__(self):
-        deployment = "Deployment: {0}\n".format(self.name)
-        return deployment + "\n".join(str(build) for build in self.builds)
+	deployment = "Deployment: {0}\n".format(self.name)
+	return deployment + "\n".join(str(build) for build in self.builds)
 
 
 class ChefDeploymentBuild(DeploymentBuild):
@@ -178,34 +178,34 @@ class ChefDeploymentBuild(DeploymentBuild):
     """
 
     def __init__(self, name, is_remote=True, builds=[], pre_commands=[],
-                 post_commands=[]):
-        super(ChefDeploymentBuild, self).__init__(name, builds=[],
-                                                  pre_commands=[],
-                                                  post_commands=[])
-        self.is_remote = is_remote
-        self.api = chef_api()
+		 post_commands=[]):
+	super(ChefDeploymentBuild, self).__init__(name, builds=[],
+						  pre_commands=[],
+						  post_commands=[])
+	self.is_remote = is_remote
+	self.api = chef_api()
 
     def _run_commands(self, commands):
-        for command in commands:
-            #If its a string run on remote server
-            if isinstance(command, (str, str)):
-                node, command = command
-                self.qa.run_command_on_node(Node(node), command)
-            if isinstance(command, dict):
-                try:
-                    func = getattr(self.qa, command['function'])
-                    kwargs = self._map_kwargs(command['kwargs'])
-                    func(**kwargs)
-                except:
-                    print traceback.print_exc()
-                    sys.exit(1)
-            #elif function run the function
-            elif hasattr(command, '__call__'):
-                command()
+	for command in commands:
+	    #If its a string run on remote server
+	    if isinstance(command, (str, str)):
+		node, command = command
+		self.qa.run_command_on_node(Node(node), command)
+	    if isinstance(command, dict):
+		try:
+		    func = getattr(self.qa, command['function'])
+		    kwargs = self._map_kwargs(command['kwargs'])
+		    func(**kwargs)
+		except:
+		    print traceback.print_exc()
+		    sys.exit(1)
+	    #elif function run the function
+	    elif hasattr(command, '__call__'):
+		command()
 
     def preconfigure(self):
-        if self.is_remote:
-            chef_server = next(b.name for b in self.builds
-                               if b.role == "chef_server")
-            self.api.server = chef_server
-            super(ChefDeploymentBuild, self).preconfigure()
+	if self.is_remote:
+	    chef_server = next(b.name for b in self.builds
+			       if b.role == "chef_server")
+	    self.api.server = chef_server
+	    super(ChefDeploymentBuild, self).preconfigure()
