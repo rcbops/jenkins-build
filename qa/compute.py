e56c9ea51c6b4b894be55d2801447322e05ced39
cjoelrun <cameronjlopez@gmail.com>
2013-10-01 19:13:23 -0500

    add test

--- qa/compute.py
+++ qa/compute.py
@@ -136,7 +136,49 @@ def destroy(name="autotest", os="precise", branch="grizzly",
     qa.cleanup_environment(env)
     qa.delete_environment(env)
 
+
+def test(environment="autotest-precise-grizzly-glance-cf",
+         razor_ip="198.101.133.3", log_level="error"):
+    """
+    Tests an openstack cluster with tempest
+    """
+    qa = rpcsqa_helper(razor_ip=razor_ip)
+    env = Environment(environment)
+    if 'remote_chef' in env.override_attributes:
+        api = qa.remote_chef_client(environment)
+        env = Environment(environment, api=api)
+    else:
+        api = qa.chef
+    query = ("chef_environment:{0} AND "
+             "(run_list:*ha-controller* OR "
+             "run_list:*single-controller*)").format(environment)
+    controllers = list(qa.node_search(query, api=api))
+    if not controllers:
+        print "No controllers in environment"
+        sys.exit(1)
+
+    for controller in controllers:
+        if 'recipe[tempest]' not in controller.run_list:
+            print "Adding tempest to controller run_list"
+            controller.run_list.append('recipe[tempest]')
+            controller.save()
+            print "Updating tempest cookbooks"
+            qa.update_tempest_cookbook(env)
+            print "Running chef-client"
+            qa.run_chef_client(controller, num_times=2,
+                               log_level=log_level)
+            cmd = "python /opt/tempest/tools/install_venv.py"
+            qa.run_command_on_node(controller, cmd)
+    qa.feature_test(controllers[0], environment)
+
+    # if len(controllers) > 1:
+    #     for i, controller in enumerate(controllers):
+    #         qa.disable_controller(controller)
+    #         time.sleep(180)
+    #         qa.test(controller[0], environment)
+    #         qa.enable_controller(controller)
+
 if __name__ == "__main__":
     parser = argh.ArghParser()
-    parser.add_commands([build, destroy])
+    parser.add_commands([build, test, destroy])
     parser.dispatch()
