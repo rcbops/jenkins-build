0a9e3e1c5c153005054b7e2f40599dab336f660b
Jacob Wagner <swagner1104@gmail.com>
2013-10-03 15:52:18 -0500

    added attr adds

--- qa/modules/Environments.py
+++ qa/modules/Environments.py
@@ -2,20 +2,11 @@
 OpenStack Environments
 """
 
-
 class Environment(dict):
 
     def __init__(self, name, description):
         self.name = name
         self.description = description
-
-    def __repr__(self):
-        """ print current instace of class
-        """
-        outl = 'class :' + self.__class__.__name__
-        for attr in self.__dict__:
-            outl += '\n\t' + attr + ' : ' + str(getattr(self, attr))
-
 
 class Chef(Environment):
 
@@ -26,3 +17,15 @@ class Chef(Environment):
         self.chef_type = "environment"
         self.default_attributes = default
         self.override_attributes = override
+
+    def _add_override_attr(self, key, value):
+        self.override_attributes[key] = value
+
+    def _add_default_attr(self, key, value):
+        self.default_attributes[key] = value
+
+    def _del_override_attr(self, key, value):
+        del self.override_attributes[key]
+
+    def _del_default_attr(self, key, value):
+        del self.default_attributes[key]
