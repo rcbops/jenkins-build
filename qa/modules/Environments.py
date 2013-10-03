6b80849432f68b9ada5625c5a983749f154276f2
Jacob Wagner <swagner1104@gmail.com>
2013-10-03 13:09:25 -0500

    chef env

--- qa/modules/Environments.py
+++ qa/modules/Environments.py
@@ -3,7 +3,7 @@ OpenStack Environments
 """
 
 
-class Environment(object):
+class Environment(dict):
 
     def __init__(self, name, description):
         self.name = name
@@ -15,3 +15,14 @@ class Environment(object):
         outl = 'class :' + self.__class__.__name__
         for attr in self.__dict__:
             outl += '\n\t' + attr + ' : ' + str(getattr(self, attr))
+
+
+class Chef(Environment):
+
+    def __init__(self, name, description, default={}, override={}):
+        super(Chef, self).__init__(name, description)
+        self.cookbook_versions = {}
+        self.json_class = "Chef::Environment"
+        self.chef_type = "environment"
+        self.default_attributes = default
+        self.override_attributes = override
