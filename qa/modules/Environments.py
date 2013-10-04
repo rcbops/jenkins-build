faf17dc6a7c72b92479b6cc70a6b93d431312f3e
Jacob Wagner <swagner1104@gmail.com>
2013-10-04 12:08:42 -0500

    added chef_server_name attr

--- qa/modules/Environments.py
+++ qa/modules/Environments.py
@@ -12,7 +12,7 @@ class Environment(dict):
 
 class Chef(Environment):
 
-    def __init__(self, name, local_api, remote_api=None, 
+    def __init__(self, name, local_api, chef_server_name, remote_api=None, 
                  description=None, default={}, override={}):
         super(Chef, self).__init__(name, description)
         self.cookbook_versions = {}
@@ -22,6 +22,7 @@ class Chef(Environment):
         self.override_attributes = override
         self.local_api = local_api
         self.remote_api = remote_api
+        self.chef_server_name = chef_server_name
 
     def _add_override_attr(self, key, value):
         self.override_attributes[key] = value
